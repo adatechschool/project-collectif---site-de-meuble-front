0620f5b60ddcda18c7cf1ea304d06873040f7f74
Zineb-Ada <85489706+Zineb-Ada@users.noreply.github.com>
2021-11-03 17:01:04 +0100

    Fetch + affichage Items

--- src/Item.js
+++ src/Item.js
@@ -1,23 +1,21 @@
 import React from 'react'
 import twitter from './twitter.svg'
 
-function Item(props) {
-    return (
+const Item = ({ info }) => 
         <div>
             <img src ={twitter} alt= 'twitter' className ='product'/>
-            <h4 className='name'> {props.name}
+            <h4 className='name'> {`Nom: ${info.name}`}
             </h4>
-            <h5 className='category'> Buffet
+            <h5 className='category'> {`Catégorie: ${info.username}`}
             </h5>
-            <h5 className='price'> 3000Mad
+            <h5 className='price'> {`Prix: ${info.email}`}
             </h5>
             <button type='button' className='button_item'>
             Acheter 
             </button>
             
         </div>
-    )
-}
+
 
 
 export default Item
--- src/Main.js
+++ src/Main.js
@@ -1,38 +1,39 @@
 import React from 'react'
 import Item from './Item'
 
-class Main extends React.Component{
-    constructor(props){
-        super(props);
+class Main extends React.Component {
+  constructor (props) {
+    super(props)
 
-        this.state = {
-            listOfItems: []
-        };
+    this.state = {
+      listOfItems: []
     }
-    componentDidMount() {
-        fetch(
-"https://jsonplaceholder.typicode.com/users")
-            .then((response) => response.json())
-            .then((json) => {
-                this.setState({
-                    listOfItems: json,
-                });
-            })
-    }
-    render(){
-        const {listOfItems} = this.state
-        listOfItems.map(item=><Item key={item.id} name={item.name} />)
-        return (
-        <div>
-            <h1>
-                Anciens meuble pour une nouvelle vie
-            </h1>
-            <div className='container'>  
-                <div className='column_items' id='column_1'> 
-                <Item />
-                </div>
-            </div>
+  }
+  componentDidMount () {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          listOfItems: json
+        })
+      })
+  }
+  render () {
+    const { listOfItems } = this.state
+
+    return (
+      <div>
+        <h1>Anciens meuble pour une nouvelle vie</h1>
+        <div className='container'>
+          <div className='column_items' id='column_1'>
+            {listOfItems.map(item => {
+              return <Item info={item} />
+            })}
+          </div>
         </div>
-    )}}
-    
+      </div>
+    )
+  }
+}
+
 export default Main
