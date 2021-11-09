86baf5b20401e7a62ec7b9c99c93940c8a4e8114
clementgibot <85489666+clementgibot@users.noreply.github.com>
2021-11-09 10:42:26 +0100

    Update Item.js

--- src/Item.js
+++ src/Item.js
@@ -9,9 +9,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const Item = ({ info }) => (
   <div>
     <img src={twitter} alt="twitter" className="product" />
-    <h4 className="name"> {`Nom: ${info.name}`}</h4>
-    <h5 className="category"> {`Catégorie: ${info.username}`}</h5>
-    <h5 className="price"> {`Prix: ${info.email}`}</h5>
+    <h4 className="name"> {`Nom: ${info.nomproduit}`}</h4>
+    <h5 className="category"> {`Catégorie: ${info.libelle_categorie}`}</h5>
+    <h5 className="price"> {`Prix: ${info.prix}`}</h5>
     <button type="button" className="button_item">
       Acheter
     </button>
